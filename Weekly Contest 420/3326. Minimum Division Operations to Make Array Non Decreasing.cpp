c2632c6c383db457577f0d575fd20f5a39d7b222
lalit pal <lalitpal@lalits-MacBook-Air.local>
2024-10-20 11:07:28 +0530

    3326

---
+++ Weekly Contest 420/3326. Minimum Division Operations to Make Array Non Decreasing.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int ans = 0;
+        for (int i = nums.size() - 1; i > 0; --i) {
+            if (nums[i] < nums[i - 1]) {
+                nums[i - 1] = get_max_proper_divisor(nums[i], nums[i - 1]);
+                if (nums[i - 1] == -1) {
+                    return -1;
+                }
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+    int get_max_proper_divisor(int n1, int n2) {
+        for (int i = n1; i > 1; i--) {
+            if (n2 % i == 0)
+                return i;
+        }
+        return -1;
+    }
+};
